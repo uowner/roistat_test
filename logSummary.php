73e2c71be72b3fdab0f9cd36a40295d7c0f30a81
uowner <uowner@gmail.com>
2021-12-29 23:41:34 +0300

    Add files via upload

--- logSummary.php
+++ logSummary.php
@@ -13,14 +13,14 @@ Class logSummary {
 		$this->views = 0;
 		$this->urls = 0;
 		$this->traffic = 0;
-		$this->crawlers = array("Google" => 0, "Bing" => 0, "Baidu" => 0, "Yandex" => 0);
-		$this->statusCodes = array("200" => 0);
+		$this->crawlers = array('Google' => 0, 'Bing' => 0, 'Baidu' => 0, 'Yandex' => 0);
+		$this->statusCodes = array('200' => 0);
 	}
 
-	function processLogString(string $logString) : void {
-		$explodedLogString = explode("\"", $logString);
-		$explodedLogString[1] = explode(" ", $explodedLogString[1]);
-		$explodedLogString[2] = explode(" ", $explodedLogString[2]);
+	public function processLogString(string $logString) : void {
+		$explodedLogString = explode('"', $logString);
+		$explodedLogString[1] = explode(' ', $explodedLogString[1]);
+		$explodedLogString[2] = explode(' ', $explodedLogString[2]);
 		$this->views++;
 		$this->traffic += (int) $explodedLogString[2][2];
 		$this->urlsData[$explodedLogString[1][1]] = 1;
@@ -33,30 +33,30 @@ Class logSummary {
 		$this->findCrawlerFingerprint($logString);
 	}
 
-	function findCrawlerFingerprint(string $logString) : void {
+	private function findCrawlerFingerprint(string $logString) : void {
 		$loweredLogString = strtolower($logString);
-		if(strpos($loweredLogString, "googlebot") > 0) {
-			$this->crawlers["Google"]++;
+		if(strpos($loweredLogString, 'googlebot') > 0) {
+			$this->crawlers['Google']++;
 		}
-		else if(strpos($loweredLogString, "bingbot") > 0) {
-			$this->crawlers["Bing"]++;
+		else if(strpos($loweredLogString, 'bingbot') > 0) {
+			$this->crawlers['Bing']++;
 		}
-		else if(strpos($loweredLogString, "baiduspider") > 0) {
-			$this->crawlers["Baidu"]++;
+		else if(strpos($loweredLogString, 'baiduspider') > 0) {
+			$this->crawlers['Baidu']++;
 		}
-		else if(strpos($loweredLogString, "yandexbot") > 0) {
-			$this->crawlers["Yandex"]++;
+		else if(strpos($loweredLogString, 'yandexbot') > 0) {
+			$this->crawlers['Yandex']++;
 		}
 	}
 
-	function printData() : void {
+	public function printData() : void {
 		echo json_encode($this);
 	}
 }
 
 $myLogSummary = new logSummary;
 
-$fileStream=fopen("access.log","r");
+$fileStream=fopen('access.log','r');
 
 while($line=fgets($fileStream)) {
         $myLogSummary->processLogString($line);
